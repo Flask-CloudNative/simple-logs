9916f4f06a7fef6e19c1382513955f7f176e80c0
hekaiyou <hekaiyou@qq.com>
2021-03-02 09:44:35 +0800

    将套接字主机改成环境变量

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from bottle import static_file, Bottle
 from bottle.ext.websocket import GeventWebSocketServer
 from bottle.ext.websocket import websocket
@@ -21,7 +22,30 @@ def api_v1_logs_websocket(ws):
 
 @app.get('/api/v1/logs')
 def api_v1_logs():
-    return static_file('logs.html', root='./html/')  # 静态文件
+    web_socket_host = os.environ.get('LOG_WEBSOCKET', '0.0.0.0:6008')
+    html_text = '''
+    <!DOCTYPE HTML>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title>Simple Logs</title>
+            <link rel="stylesheet" href="/api/v1/logs/static/logs.css" type="text/css" />
+            <script type="text/javascript">
+                var wsUrl = "ws://{0}/api/v1/logs/websocket";
+            </script>
+            <script src="/api/v1/logs/static/logs.js" type="text/javascript" charset="utf-8"></script>
+        </head>
+        <body>
+            <pre class="msg"><code></code></pre>
+        </body>
+    </html>
+    '''.format(web_socket_host)
+    return html_text
+
+
+@app.get('/api/v1/logs/static/<path>')
+def api_v1_logs_static(path):
+    return static_file(path, root='./static/')  # 静态文件
 
 
 if __name__ == '__main__':
