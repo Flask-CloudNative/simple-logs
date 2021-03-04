8b5d661b25e533de97159036b5362ff97aa0e22c
hekaiyou <hekaiyou@qq.com>
2021-03-04 15:27:42 +0800

    按应用和组件分类管理日志

--- app.py
+++ app.py
@@ -4,13 +4,19 @@ from gevent import pywsgi
 from geventwebsocket.handler import WebSocketHandler
 
 app = Flask(__name__)
-users = set()  # 连接进来的WebSocket客户端集合
+users = {}  # 连接进来的WebSocket客户端集合（按应用区分）
+users_key = []  # 供用户选择要查看那个应用的日志
 
 
-@app.route('/logs/websocket')
-def logs_websocket():
+@app.route('/logs/websocket/<string:application>/<string:component>')
+def logs_websocket(application, component):
     wsock = request.environ.get('wsgi.websocket')
-    users.add(wsock)
+    # 判断这个应用是否已存在，不存在时就创建应该新集合
+    if not users.__contains__(application):
+        users[application] = set()
+        users_key.append(application)
+        print('应用 {0} 完成注册, 客户端集合已创建!'.format(application))
+    users[application].add(wsock)
     if not wsock:
         abort(400, 'Expected WebSocket request.')
     while True:
@@ -19,19 +25,24 @@ def logs_websocket():
         except Exception:
             break
         if message:
-            for user in users:
+            for user in users[application]:
                 try:
-                    user.send(message)  # 给客户端推送信息
+                    # 给客户端推送信息
+                    user.send('{0}> {1}'.format(component, message))
                 except Exception:
                     break  # 客户端已断开连接
-    users.remove(wsock)  # 如果有客户端断开，则删除这个断开的WebSocket
+    users[application].remove(wsock)  # 如果有客户端断开，则删除这个断开的WebSocket
     return 'Client has broken the connection.'
 
 
-@app.route('/logs')
-def logs():
+@app.route('/logs/view/<string:application>')
+def logs(application):
     web_socket_host = os.environ.get('LOG_WEBSOCKET', 'localhost:6008')
-    return render_template('logs.html', web_socket_host=web_socket_host)
+    if application in users_key:
+        app_name = application
+    else:
+        app_name = None
+    return render_template('logs.html', web_socket_host=web_socket_host, users_key=users_key, application=app_name)
 
 
 if __name__ == '__main__':
